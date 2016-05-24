8b61c3aeb5c921234d1836c28a93ca45f0be6571
Gail Steiger <gsteiger@netvision.net.il>
2016-05-24 18:14:33 +0300

    development

--- index.js
+++ index.js
@@ -4,17 +4,56 @@
 const validator = require('url-validator');
 
 const fs = require('fs');
+const _ = require('lodash');
 
 const EventEmitter = require('events');
 
+exports.process = process;
 
-const myEmitter = new EventEmitter();
+function process(filename) {
 
-exports.process = function (fileName) {
-    fs.readFile(filename, function (err, data) {
-        if (err) throw err;
-        console.log(data);
+    // create emitter
+    const myEmitter = new EventEmitter();
+
+    // emit start
+    myEmitter.emit('start');
+
+    const stream = fs.createReadStream(filename, {encoding: 'utf-8'});
+
+    var buf = '';
+
+    stream.on('data', function (chunk) {
+        buf += chunk;
+    });
+
+    stream.on('error', function () {
+        myEmitter.emit('error');
+    });
+
+    stream.on('end', function () {
+        var urls = buf.split('\n');
+        evaluate(urls, myEmitter);
+        
+        myEmitter.emit('end');
+        stream.close();
+    });
+
+    return myEmitter;
+    
+}
+
+function evaluate(urls, emitter) {
+    _.forEach(urls, function (url) {
+        var result = validator.parse(url);
+        
+        if(!result) {
+            emitter.emit('data-error', url);
+        } else {
+            emitter.emit('data', result);
+        }
     });
 }
+
+
 
 
